96434ca21a23eba67f78b97ca5a663a5b0dd2a16
Serdar Ayalp <serdar.ayalp@regiocom.com>
2023-05-05 17:23:34 +0200

    Fundament

--- src/main/java/de/mydomain/CustomEventListenerProvider.java
+++ src/main/java/de/mydomain/CustomEventListenerProvider.java
@@ -7,21 +7,25 @@ import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.RealmProvider;
-import org.keycloak.models.UserModel;
+import org.keycloak.models.*;
+import org.keycloak.theme.Theme;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Properties;
 
 public class CustomEventListenerProvider implements EventListenerProvider {
 
-    private static final Logger log = Logger.getLogger(CustomEventListenerProvider.class);
+    private static final Logger logger = Logger.getLogger(CustomEventListenerProvider.class);
 
-    private final KeycloakSession session;
-    private final RealmProvider model;
+    private final KeycloakSession keycloakSession;
+    private final RealmProvider realmProvider;
 
-    public CustomEventListenerProvider(KeycloakSession session) {
-        this.session = session;
-        this.model = session.realms();
+    public CustomEventListenerProvider(KeycloakSession keycloakSession) {
+        this.keycloakSession = keycloakSession;
+        this.realmProvider = keycloakSession.realms();
     }
 
     @Override
@@ -29,40 +33,47 @@ public class CustomEventListenerProvider implements EventListenerProvider {
 
         if (EventType.REGISTER.equals(event.getType())) {
 
-            log.info("*************************************************************************");
+            RealmModel realm = realmProvider.getRealm(event.getRealmId());
+            UserModel newUser = keycloakSession.users().getUserById(realm, event.getUserId());
 
-            log.infof("######### NEW [%s] EVENT", event.getType());
+            sendWelcomeMail(realm, newUser);
+
+            /*
+            logger.info("*************************************************************************");
+
+            logger.infof("######### NEW [%s] EVENT", event.getType());
 
             RealmModel realm = model.getRealm(event.getRealmId());
-            UserModel newRegisteredUser = session.users().getUserById(realm, event.getUserId());
+            UserModel newUser = keycloakSession.users().getUserById(realm, event.getUserId());
 
             String emailPlainContent = "New user registration\n\n" +
-                    "Email: " + newRegisteredUser.getEmail() + "\n" +
-                    "Username: " + newRegisteredUser.getUsername() + "\n" +
+                    "Email: " + newUser.getEmail() + "\n" +
+                    "Username: " + newUser.getUsername() + "\n" +
                     "Client: " + event.getClientId();
 
             String emailHtmlContent = "<h1>New user registration</h1>" +
                     "<ul>" +
-                    "<li>Email: " + newRegisteredUser.getEmail() + "</li>" +
-                    "<li>Username: " + newRegisteredUser.getUsername() + "</li>" +
+                    "<li>Email: " + newUser.getEmail() + "</li>" +
+                    "<li>Username: " + newUser.getUsername() + "</li>" +
                     "<li>Client: " + event.getClientId() + "</li>" +
                     "</ul>";
 
-            DefaultEmailSenderProvider senderProvider = new DefaultEmailSenderProvider(session);
+            DefaultEmailSenderProvider senderProvider = new DefaultEmailSenderProvider(keycloakSession);
 
             try {
                 senderProvider.send(
-                        session.getContext().getRealm().getSmtpConfig(),
+                        keycloakSession.getContext().getRealm().getSmtpConfig(),
                         "admin@example.com",
                         "Keycloak - New Registration",
                         emailPlainContent,
                         emailHtmlContent);
 
             } catch (EmailException e) {
-                log.error("Failed to send email", e);
+                logger.error("Failed to send email", e);
             }
 
-            log.info("*************************************************************************");
+            logger.info("*************************************************************************");
+            */
         }
 
     }
@@ -73,5 +84,53 @@ public class CustomEventListenerProvider implements EventListenerProvider {
 
     @Override
     public void close() {
+    }
+
+    private void sendWelcomeMail(RealmModel realmModel, UserModel userModel) {
+
+        final String welcomeMailSubject = getMessagesFromKeycloakTheme(realmModel, userModel, "welcomeMailSubject");
+        final String welcomeTextMail = getMessagesFromKeycloakTheme(realmModel, userModel, "welcomeMailBody");
+        final String welcomeHtmlMail = getMessagesFromKeycloakTheme(realmModel, userModel, "welcomeMailBodyHtml");
+
+        DefaultEmailSenderProvider senderProvider = new DefaultEmailSenderProvider(keycloakSession);
+        try {
+            senderProvider.send(realmModel.getSmtpConfig(),
+                    userModel,
+                    welcomeMailSubject,
+                    welcomeTextMail,
+                    welcomeHtmlMail);
+        } catch (EmailException e) {
+            logger.error("Error Sending Email.", e);
+        }
+    }
+
+    private String getMessagesFromKeycloakTheme(RealmModel realmModel, UserModel userModel, String messageSource) {
+
+        Theme theme = getTheme(realmModel, userModel);
+        Locale locale = getLocale(userModel);
+
+        try {
+            Properties p = theme.getMessages(locale);
+            return new MessageFormat(p.getProperty(messageSource, messageSource), locale).format(Collections.emptyList().toArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Theme getTheme(RealmModel realmModel, UserModel userModel) {
+        // ThemeProvider tp = keycloakSession.getProvider(ThemeProvider.class, "extending");
+        ThemeManager themeManager = keycloakSession.theme();
+        try {
+            return themeManager.getTheme(realmModel.getEmailTheme(), Theme.Type.EMAIL);
+            // return tp.getTheme(realmModel.getEmailTheme(), Theme.Type.EMAIL);
+        } catch (IOException e) {
+            logger.error("Error retrieving email theme", e);
+        }
+        return null;
+    }
+
+    private Locale getLocale(UserModel userModel) {
+        return keycloakSession.getContext().resolveLocale(userModel);
     }
 }
